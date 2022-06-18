0c82928d5d17cf32c74093b74ef8a378e6389e6e
Ítalo Vinícius de Freitas Dias <italo.dias.017@ufrn.edu.br>
2022-06-18 20:38:11 -0300

    Finishing the login with google

--- lib/src/presentation/pages/login_screen.dart
+++ lib/src/presentation/pages/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get_it/get_it.dart';
 import 'package:notepad_app/src/presentation/components/custom_buttom.dart';
@@ -24,99 +25,102 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          child: store.isLoading
-              ? Center(
-                  child: CircularProgressIndicator.adaptive(
-                    valueColor: AlwaysStoppedAnimation<Color>(
-                      CustomColors.primaryColor,
+        child: Observer(builder: (context) {
+          return Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: store.isLoading
+                ? Center(
+                    child: CircularProgressIndicator.adaptive(
+                      valueColor: AlwaysStoppedAnimation<Color>(
+                        CustomColors.primaryColor,
+                      ),
                     ),
-                  ),
-                )
-              : Column(
-                  children: [
-                    Expanded(
-                      child: ClipPath(
-                        clipper: MyClipper(),
-                        child: Container(
-                          decoration: const BoxDecoration(
-                            image: DecorationImage(
-                              image: AssetImage("assets/full-bloom.png"),
-                              fit: BoxFit.cover,
+                  )
+                : Column(
+                    children: [
+                      Expanded(
+                        child: ClipPath(
+                          clipper: MyClipper(),
+                          child: Container(
+                            decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage("assets/full-bloom.png"),
+                                fit: BoxFit.cover,
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    Expanded(
-                      child: Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(
-                                left: 15.0, right: 15.0, bottom: 20),
-                            child: CustomTextField(
-                              labelText: "E-mail",
-                              isPassword: false,
-                              placeholder: "Digite seu e-mail",
-                              controller: emailController,
-                              icon: const Icon(Icons.person_outline),
-                            ),
-                          ),
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(left: 15.0, right: 15.0),
-                            child: CustomTextField(
-                              labelText: "Senha",
-                              placeholder: "Digite sua senha",
-                              isPassword: true,
-                              controller: passController,
-                              icon: const Icon(Icons.vpn_key_rounded),
-                            ),
-                          ),
-                          const SizedBox(height: 25),
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(left: 15.0, right: 15.0),
-                            child: CustomButtom(
-                              text: "LOGIN",
-                              backgroundColor: CustomColors.primaryColor,
-                            ),
-                          ),
-                          const SizedBox(height: 15),
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(left: 15.0, right: 15.0),
-                            child: CustomIconButtom(
-                              icon: const Icon(
-                                FontAwesomeIcons.google,
-                                color: Colors.white,
+                      Expanded(
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 15.0, right: 15.0, bottom: 20),
+                              child: CustomTextField(
+                                labelText: "E-mail",
+                                isPassword: false,
+                                placeholder: "Digite seu e-mail",
+                                controller: emailController,
+                                icon: const Icon(Icons.person_outline),
                               ),
-                              text: "LOGIN COM GOOGLE",
-                              backgroundColor: Colors.redAccent,
-                              onPressed: () async {
-                                await store.googleLogin(context);
-                              },
                             ),
-                          ),
-                          const SizedBox(height: 15),
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(left: 15.0, right: 15.0),
-                            child: CustomButtom(
-                              text: "Não possui uma conta?",
-                              backgroundColor: Colors.white,
-                              textColor: CustomColors.primaryColor,
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 15.0, right: 15.0),
+                              child: CustomTextField(
+                                labelText: "Senha",
+                                placeholder: "Digite sua senha",
+                                isPassword: true,
+                                controller: passController,
+                                icon: const Icon(Icons.vpn_key_rounded),
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-        ),
+                            const SizedBox(height: 25),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 15.0, right: 15.0),
+                              child: CustomButtom(
+                                text: "Login",
+                                backgroundColor: CustomColors.primaryColor,
+                              ),
+                            ),
+                            const SizedBox(height: 15),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 15.0, right: 15.0),
+                              child: CustomIconButtom(
+                                icon: const Icon(
+                                  FontAwesomeIcons.google,
+                                  color: Colors.white,
+                                ),
+                                text: "Login com google",
+                                backgroundColor: Colors.redAccent,
+                                onPressed: () async {
+                                  await store.googleLogin(context);
+                                },
+                              ),
+                            ),
+                            const SizedBox(height: 15),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 15.0, right: 15.0),
+                              child: CustomButtom(
+                                text: "Não possui uma conta?",
+                                backgroundColor: Colors.white,
+                                textColor: CustomColors.primaryColor,
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+          );
+        }),
       ),
     );
   }
@@ -125,7 +129,7 @@ class _LoginScreenState extends State<LoginScreen> {
 class MyClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
-    Path p = new Path();
+    Path p = Path();
     p.lineTo(size.width, 0.0);
     p.lineTo(size.width, size.height * 0.85);
     p.arcToPoint(
